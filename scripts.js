457780286844447c5030bae1e44496ab2fa2ae62
Cedrick Monare <162999186+CedrickMonare@users.noreply.github.com>
2024-07-19 12:56:05 +0200

    created an object to manipulate the DOM

--- scripts.js
+++ scripts.js
@@ -1,26 +1,22 @@
-import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
+import { books, authors, genres, BOOKS_PER_PAGE } from './data.js';
 
-let page = 1;
-let matches = books
+//The BookInteraction object contains utility methods for manipulating the DOM and updating the book-related UI elements.
+const BookInteraction = {
+    createBookPreview({ author, id, image, title }) {
+        const element = document.createElement('button');
+        element.classList = 'preview';
+        element.setAttribute('data-preview', id);
 
-const starting = document.createDocumentFragment()
+        element.innerHTML = `
+            <img class="preview__image" src="${image}" />
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `;
 
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-    const element = document.createElement('button')
-    element.classList = 'preview'
-    element.setAttribute('data-preview', id)
-
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `
+        return element;
+    },
 
     starting.appendChild(element)
 }
