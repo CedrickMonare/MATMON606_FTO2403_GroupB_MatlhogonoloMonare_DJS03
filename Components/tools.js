2c59542e2551c9991e7283e8139b338dee77e096
Cedrick Monare <162999186+CedrickMonare@users.noreply.github.com>
2024-07-19 15:28:28 +0200

    Added attribute values and defined the new element to be used

--- Components/tools.js
+++ Components/tools.js
@@ -23,3 +23,59 @@ class BookPreview extends HTMLElement {
       // Initial rendering of the component.
       this.render();
     }
+
+render() {
+      // Attribute values from the element.
+      const author = this.getAttribute("author");
+      const id = this.getAttribute("id");
+      const image = this.getAttribute("image");
+      const title = this.getAttribute("title");
+  
+      // inner HTML of the shadow root to define the component's structure and styling.
+      this.shadowRoot.innerHTML = `     
+          <style>
+            .preview {
+              display: flex;
+              align-items: center;
+              cursor: pointer;
+              border: none;
+              background: none;
+              padding: 10px;
+            }
+    
+            .preview__image {
+              width: 50px;
+              height: 75px;
+              object-fit: cover;
+              margin-right: 10px;
+            }
+    
+            .preview__info {
+              display: flex;
+              flex-direction: column;
+            }
+    
+            .preview__title {
+              font-size: 1rem;
+              margin: 0;
+            }
+    
+            .preview__author {
+              font-size: 0.875rem;
+              color: gray;
+            }
+          </style>
+    
+          <button class="preview" data-preview="${id}">
+            <img class="preview__image" src="${image}" />
+            <div class="preview__info">
+              <h3 class="preview__title">${title}</h3>
+              <div class="preview__author">${author}</div>
+            </div>
+          </button>
+        `;
+    }
+  }
+  
+  // Defines the new element so it can be used in the HTML.
+  customElements.define("book-preview", BookPreview);
