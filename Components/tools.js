98cfc7d608684efd71604195592d436dcc5cbb5f
Cedrick Monare <162999186+CedrickMonare@users.noreply.github.com>
2024-07-19 15:26:25 +0200

    Specified attributes to monitor for change and added a constructor

--- Components/tools.js
+++ Components/tools.js
@@ -1,66 +1,25 @@
-// book-preview.js
-import { authors } from './data.js';
-
 class BookPreview extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        const template = document.createElement('template');
-        template.innerHTML = `
-            <style>
-                .preview {
-                    /* Add your styles here */
-                }
-                .preview__image {
-                    width: 100px;
-                    height: auto;
-                }
-                .preview__info {
-                    display: flex;
-                    flex-direction: column;
-                }
-                .preview__title {
-                    font-size: 16px;
-                    margin: 0;
-                }
-                .preview__author {
-                    font-size: 12px;
-                    color: grey;
-                }
-            </style>
-            <button class="preview">
-                <img class="preview__image" />
-                <div class="preview__info">
-                    <h3 class="preview__title"></h3>
-                    <div class="preview__author"></div>
-                </div>
-            </button>
-        `;
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-    }
-
-    connectedCallback() {
-        this.shadowRoot.querySelector('.preview__image').src = this.getAttribute('image');
-        this.shadowRoot.querySelector('.preview__title').innerText = this.getAttribute('title');
-        this.shadowRoot.querySelector('.preview__author').innerText = authors[this.getAttribute('author')];
-        this.setAttribute('data-preview', this.getAttribute('id'));
-    }
-
+ 
     static get observedAttributes() {
-        return ['image', 'title', 'author', 'id'];
+      // These are the attributes that will be monitored for changes.
+      return ["author", "id", "image", "title"];
     }
-
+      
+    //Constructs an instance of the BookPreview element.
+      // Attached a shadow DOM tree to this instance in open mode.
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+    }
+  
     attributeChangedCallback(name, oldValue, newValue) {
-        if (name === 'image') {
-            this.shadowRoot.querySelector('.preview__image').src = newValue;
-        }
-        if (name === 'title') {
-            this.shadowRoot.querySelector('.preview__title').innerText = newValue;
-        }
-        if (name === 'author') {
-            this.shadowRoot.querySelector('.preview__author').innerText = authors[newValue];
-        }
+      // Only update if the attribute's value has actually changed.
+      if (oldValue !== newValue) {
+        this.render();
+      }
     }
-}
-
-customElements.define('book-preview', BookPreview);
+   
+    connectedCallback() {
+      // Initial rendering of the component.
+      this.render();
+    }
